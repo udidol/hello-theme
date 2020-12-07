b52a8c966fb56991cc7f6111de36e12a4a9d8630
Marc Perel <marc@obox.co.za>
2020-12-07 21:28:55 +0200

    Added additional footer components

--- template-parts/footer.php
+++ template-parts/footer.php
@@ -8,7 +8,38 @@
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
+$site_name = get_bloginfo( 'name' );
+$tagline   = get_bloginfo( 'description', 'display' );
 ?>
 <footer id="site-footer" class="site-footer" role="contentinfo">
-	<?php // footer. ?>
+	<div class="site-branding">
+		<?php
+		if ( has_custom_logo() ) {
+			the_custom_logo();
+		} elseif ( $site_name ) {
+			?>
+			<h4 class="site-title">
+				<a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php esc_attr_e( 'Home', 'hello-elementor' ); ?>" rel="home">
+					<?php echo esc_html( $site_name ); ?>
+				</a>
+			</h4>
+			<p class="site-description">
+				<?php
+				if ( $tagline ) {
+					echo esc_html( $tagline );
+				}
+				?>
+			</p>
+		<?php } ?>
+	</div>
+
+	<?php if ( has_nav_menu( 'menu-1' ) ) : ?>
+		<nav class="site-navigation" role="navigation">
+			<?php wp_nav_menu( array( 'theme_location' => 'menu-1', 'depth' => 1 ) ); ?>
+		</nav>
+	<?php endif; ?>
+
+	<div class="copyright">
+		<p><?php _e('All rights reserved', 'hello-elementor'); ?></p>
+	</div>
 </footer>
